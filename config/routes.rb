097cc2e9e87c57edafef5ffe6487c276095168da
EC2 Default User <ec2-user@ip-172-31-47-57.us-east-2.compute.internal>
2018-04-06 12:40:38 +0000

    I hope this works now

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+  
+
+  get 'static_pages/about'
+
+  get 'static_pages/gallery'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
