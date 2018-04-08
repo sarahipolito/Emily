50bc8644b7ea0c32dfb9f3f2d410e7ee51143706
EC2 Default User <ec2-user@ip-172-31-47-57.us-east-2.compute.internal>
2018-04-08 08:47:55 +0000

    navigation bar working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
+  get '/about', to:'static_pages#about'
 
-  get 'static_pages/about'
-
-  get 'static_pages/gallery'
+  get '/gallery', to:'static_pages#gallery'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
