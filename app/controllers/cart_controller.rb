8ac28216d9e3c26fafbc216a4dad5c338f4aa99c
Sara Garcia Hipolito <36889311+sarahipolito@users.noreply.github.com>
2018-04-20 14:27:50 +0100

    fri2

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,65 @@
 class CartController < ApplicationController
+def add 
+    # get id of gallery
+    id = params[:id]
+  
+    #if cart session is already working created use existing session
+  
+    if session[:cart] then 
+      cart = session [:cart]
+    else
+      session[:cart] = {}
+      cart = session[:cart]
+    end
+  
+      
+      # check if the is already in cart if so increment by
+      
+  
+    if cart[id] then
+      cart[id] = cart[id] + 1
+      else
+        cart[id] = 1
+    end
+  
+  redirect_to :action => :index
+  
+  end 
+ 
+ 
+ def clearCart
+  # clear cart and remove all items
+  session[:cart] = nil
+  redirect_to :action => :items
+ end
+ 
+  
+  
+  
+  def index
+  
+  # pass the cart information to the index view
+  if session[:cart] then
+    @cart = session[:cart]
+  else
+    @cart = {}
+  end
+
+  end
+
+
+  def remove
+  
+  id = params[:id]
+  cart = session[:cart]
+  cart.delete id
+  
+  redirect_to :action => :index
+  end
+
+
+
+
 end
+
+  
